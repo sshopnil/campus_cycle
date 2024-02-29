f1a25ceb8003e4ef4c48b2e3af82c3e4e8bd2d7f
Shayanta Shopnil <shopnil710@gmail.com>
2024-03-01 01:00:52 +0600

    init commit

---
+++ campus_cycle/src/App.js
@@ -0,0 +1,97 @@
+import { useState, useEffect, useMemo } from "react";
+
+// react-router components
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Icon from "@mui/material/Icon";
+
+// Soft UI Dashboard React components
+import SoftBox from "components/SoftBox";
+
+// Soft UI Dashboard React examples
+import Sidenav from "examples/Sidenav";
+import Configurator from "examples/Configurator";
+
+// Soft UI Dashboard React themes
+import theme from "assets/theme";
+
+// Soft UI Dashboard React routes
+import routes from "routes";
+
+// Soft UI Dashboard React contexts
+import { useSoftUIController, setMiniSidenav, setOpenConfigurator } from "context";
+
+// Images
+import brand from "assets/images/logo-ct.png";
+
+export default function App() {
+  const [controller, dispatch] = useSoftUIController();
+  const { miniSidenav, direction, layout, openConfigurator, sidenavColor} = controller;
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  // const [loginCache, setLoginCache] = useState(null);
+  const { pathname } = useLocation();
+
+  // Open sidenav when mouse enter on mini sidenav
+  const handleOnMouseEnter = () => {
+    if (miniSidenav && !onMouseEnter) {
+      setMiniSidenav(dispatch, false);
+      setOnMouseEnter(true);
+    }
+  };
+
+  // Close sidenav when mouse leave mini sidenav
+  const handleOnMouseLeave = () => {
+    if (onMouseEnter) {
+      setMiniSidenav(dispatch, true);
+      setOnMouseEnter(false);
+    }
+  };
+
+  // Change the openConfigurator state
+  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
+
+  // Setting the dir attribute for the body element
+  // useEffect(() => {
+  //   document.body.setAttribute("dir", direction);
+  // }, [direction]);
+
+  // Setting page scroll to 0 when changing the route
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
+  const login = localStorage.getItem("user");
+  return (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    {login && <Sidenav
+      color={sidenavColor}
+      brand={brand}
+      brandName="CampusCycle"
+      routes={routes}
+      onMouseEnter={handleOnMouseEnter}
+      onMouseLeave={handleOnMouseLeave}
+    />}
+    <Routes>
+      {getRoutes(routes)}
+      <Route path="*" element={<Navigate to={login ? "/dashboard" : "/authentication/sign-in"} />} />
+    </Routes>
+  </ThemeProvider>
+  );
+}
