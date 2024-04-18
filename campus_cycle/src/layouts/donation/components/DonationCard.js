d03480f37b89ebcc6c90700c4b8a216b6f048f37
Sahadat Islam Evan <126870464+combo666@users.noreply.github.com>
2024-04-18 12:25:57 +0600

    Small button changes

--- campus_cycle/src/layouts/donation/components/DonationCard.js
+++ campus_cycle/src/layouts/donation/components/DonationCard.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Grid, Button } from "@mui/material";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 import jsonData from "../data/data.json";
 import img from "../data/bruce-mars.jpg";
 import ProgressBar from "@ramonak/react-progress-bar";
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
 import Link from "@mui/material/Link";
 
 const DonationCard = () => {
@@ -32,30 +32,40 @@ const DonationCard = () => {
     <>
       {visibleCards.map((card) => (
         <Grid key={card.id} item xs={12} md={4} style={{ marginBottom: "40px" }}>
-          <div onClick={() => handleCardClick(card.id)} style={{ cursor: 'pointer' }}>
+          <div onClick={() => handleCardClick(card.id)} style={{ cursor: "pointer" }}>
             <Card
               sx={{
                 maxWidth: 280,
-                backgroundColor: 'rgba(255, 255, 255, 0.5)',
-                backdropFilter: 'blur(10px)',
-                borderRadius: '10px',
-                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                height: 350,
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                backdropFilter: "blur(10px)",
+                borderRadius: "10px",
+                position: "relative",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
               }}
             >
-              <CardMedia
-                component="img"
-                alt="Image"
-                height="180"
-                image={img}
-              />
+              <CardMedia component="img" alt="Image" height="180" image={img} />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div" style={{ marginBottom: '20px' }}>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  style={{ marginBottom: "20px" }}
+                >
                   {card.title}
                 </Typography>
-                <Typography style={{ marginBottom: '8px', fontSize: '0.9rem' }}>
+                <Typography style={{ marginBottom: "8px", fontSize: "0.9rem" }}>
                   ${card.progress.goal} of ${card.progress.current} is raised
                 </Typography>
-                <ProgressBar completed={(card.progress.current / card.progress.goal) * 100} bgColor='#17c1e8' height='6px' labelSize='0px' borderRadius='8px' customLabel={card.progress.current} />
+                <ProgressBar
+                  completed={(card.progress.current / card.progress.goal) * 100}
+                  bgColor="#17c1e8"
+                  height="6px"
+                  labelSize="0px"
+                  borderRadius="8px"
+                  customLabel={card.progress.current}
+                  style={{ position: "absolute", bottom: "0", left: "0", right: "0" }}
+                />
               </CardContent>
             </Card>
           </div>
@@ -70,7 +80,7 @@ const DonationCard = () => {
                 setShowMore(remainingCardData);
                 setShowAll(true);
               } else {
-                setShowMore((remainingCardData) < 6 ? remainingCardData : showMore + 6);
+                setShowMore(remainingCardData < 6 ? remainingCardData : showMore + 6);
                 setRemainingCardData(remainingCardData - showMore);
               }
             }}
@@ -81,15 +91,20 @@ const DonationCard = () => {
         </Grid>
       ) : (
         <Grid container justifyContent="center" mt={2}>
-          <Button onClick={() => {
-            setShowAll(false);
-            setShowMore(3);
-            setRemainingCardData(cardsData.length);
-          }} variant="secondary">See Less</Button>
+          <Button
+            onClick={() => {
+              setShowAll(false);
+              setShowMore(3);
+              setRemainingCardData(cardsData.length);
+            }}
+            variant="secondary"
+          >
+            See Less
+          </Button>
         </Grid>
       )}
     </>
   );
-}
+};
 
 export default DonationCard;
--- campus_cycle/src/layouts/donation/components/SideBar.js
+++ campus_cycle/src/layouts/donation/components/SideBar.js
@@ -1,72 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader'; // Add this import
-import Avatar from '@mui/material/Avatar'; // Add this import
-import RecordVoiceOverIcon from '@mui/icons-material/RecordVoiceOver';
-import PaidIcon from '@mui/icons-material/Paid';
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import GroupIcon from '@mui/icons-material/Group';
-import DataSaverOffIcon from '@mui/icons-material/DataSaverOff';
-
+import React, { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader"; // Add this import
+import Avatar from "@mui/material/Avatar"; // Add this import
+import RecordVoiceOverIcon from "@mui/icons-material/RecordVoiceOver";
+import PaidIcon from "@mui/icons-material/Paid";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import GroupIcon from "@mui/icons-material/Group";
+import DataSaverOffIcon from "@mui/icons-material/DataSaverOff";
 
 const SideBar = () => {
+  const groups = [
+    { name: "Group 1", color: "#ff0000" },
+    { name: "Group 2", color: "#00ff00" },
+    { name: "Group 3", color: "#0000ff" },
+  ];
 
-    const groups = [
-        { name: "Group 1", color: "#ff0000" },
-        { name: "Group 2", color: "#00ff00" },
-        { name: "Group 3", color: "#0000ff" }
-      ];
-      
   return (
-    <Card sx={{ minWidth: "300px"}}>
-        <CardHeader
-            sx={{
-                alignSelf:"center",
-                textTransform: 'uppercase',
-                fontWeight:"bold"
-            }}
-            avatar={
-                <RecordVoiceOverIcon fontSize="200px"/>
-            }
-            title="Top Donors"
-        />
-        <hr style={{width: '80%', alignSelf: 'center', color:"#666C8F", border: '1px solid'}}/>
-        <CardContent style={{ height: '400px', overflowY: 'auto' }}>
+    <Card sx={{ minWidth: "300px" }}>
+      <CardHeader
+        sx={{
+          alignSelf: "center",
+          textTransform: "uppercase",
+          fontWeight: "bold",
+        }}
+        avatar={<RecordVoiceOverIcon fontSize="200px" />}
+        title="Top Donors"
+      />
+      <hr style={{ width: "80%", alignSelf: "center", color: "#666C8F", border: "1px solid" }} />
+      <CardContent style={{ height: "400px", overflowY: "auto" }}>
         {groups.map((item) => {
-            return <CardHeader
-                avatar={
-                    <Avatar sx={{ bgcolor: item.color, fontVariant:'small-caps'}} aria-label="Group logo">
-                        {item.name.charAt(0)}
-                    </Avatar>
-                }
-                key={item.name}
-                title={item.name}
+          return (
+            <CardHeader
+              avatar={
+                <Avatar
+                  sx={{ bgcolor: item.color, fontVariant: "small-caps" }}
+                  aria-label="Group logo"
+                >
+                  {item.name.charAt(0)}
+                </Avatar>
+              }
+              key={item.name}
+              title={item.name}
             />
+          );
         })}
-        </CardContent>
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px', padding: '10px' }}>
+      </CardContent>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px", padding: "10px" }}
+      >
         <div className="glass-box glass-box-content">
-            <div><PaidIcon />Total donation</div>
-            {/* Add content for Total donation */}
-        </div>
-        <div className="glass-box glass-box-content">
-            <div><CalendarTodayIcon />Donation today</div>
-            {/* Add content for Donation today */}
-        </div>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
-        <div className="glass-box glass-box-content">
-            <div><GroupIcon />Total donner</div>
-            {/* Add content for Total donner */}
+          <div style={{ width: "85px", height: "95px" }}>
+            <PaidIcon />
+            <div style={{ fontSize: "12px", fontWeight: 'bold'}}>Total donation</div>
+          </div>
+          {/* Add content for Total donation */}
         </div>
         <div className="glass-box glass-box-content">
-            <div><DataSaverOffIcon />Avg donation</div>
-            {/* Add content for Avg donation */}
+          <div style={{ width: "85px", height: "95px" }}>
+            <CalendarTodayIcon />
+            <div style={{ fontSize: "12px" }}>Donation today</div>
+          </div>
+          {/* Add content for Donation today */}
         </div>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center", padding: "10px" }}>
+        <div className="glass-box glass-box-content">
+          <div style={{ width: "85px", height: "95px" }}>
+            <GroupIcon />
+            <div style={{ fontSize: "12px" }}>Total donner</div>
+          </div>
+          {/* Add content for Total donner */}
         </div>
+        <div className="glass-box glass-box-content">
+          <div style={{ width: "85px", height: "95px" }}>
+            <DataSaverOffIcon />
+            <div style={{ fontSize: "12px" }}>Avg donation</div>
+          </div>
+          {/* Add content for Avg donation */}
+        </div>
+      </div>
     </Card>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
