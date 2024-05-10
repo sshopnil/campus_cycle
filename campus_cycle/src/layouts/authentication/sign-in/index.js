ff9b7caf149a40026f55767660573b49c10fe6a7
sshopnil <shopnil710@gmail.com>
2024-05-10 18:59:21 +0600

    sign in / up updated

--- campus_cycle/src/layouts/authentication/sign-in/index.js
+++ campus_cycle/src/layouts/authentication/sign-in/index.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 // @mui material components
 import Switch from "@mui/material/Switch";
+import axios from "axios";
 
 // Soft UI Dashboard React components
 import SoftBox from "components/SoftBox";
@@ -18,70 +19,122 @@ import CoverLayout from "layouts/authentication/components/CoverLayout";
 
 // Images
 import curved9 from "assets/images/curved-images/curved-6.jpg";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import LOCAL_ADDR from "GLOBAL_ADDRESS";
+import { useNavigate } from 'react-router-dom';
+
 
 function SignIn() {
   const [rememberMe, setRememberMe] = useState(true);
   const handleSetRememberMe = () => setRememberMe(!rememberMe);
+  const navigate = useNavigate();
 
-  return (
-    <CoverLayout
-      title="Welcome back"
-      description="Enter your email and password to sign in"
-      image={curved9}
-    >
-      <SoftBox component="form" role="form">
-        <SoftBox mb={2}>
-          <SoftBox mb={1} ml={0.5}>
-            <SoftTypography component="label" variant="caption" fontWeight="bold">
-              Email
-            </SoftTypography>
-          </SoftBox>
-          <SoftInput type="email" placeholder="Email" />
-        </SoftBox>
-        <SoftBox mb={2}>
-          <SoftBox mb={1} ml={0.5}>
-            <SoftTypography component="label" variant="caption" fontWeight="bold">
-              Password
-            </SoftTypography>
-          </SoftBox>
-          <SoftInput type="password" placeholder="Password" />
-        </SoftBox>
-        <SoftBox display="flex" alignItems="center">
-          <Switch checked={rememberMe} onChange={handleSetRememberMe} />
-          <SoftTypography
-            variant="button"
-            fontWeight="regular"
-            onClick={handleSetRememberMe}
-            sx={{ cursor: "pointer", userSelect: "none" }}
-          >
-            &nbsp;&nbsp;Remember me
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    // fetchData();
+    // console.log(pass);
+  };
+  // onClick = {()=> localStorage.setItem("user", "user")} 
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const response = await axios.post(`${LOCAL_ADDR}users/signin`, formData);
+      // console.log(response.status);
+        toast('Successfully signed in😆!');
+        console.log(response.data.id);
+        localStorage.setItem("user", response.data.id);
+        const delayInMilliseconds = 3000;
+
+        const timeoutId = setTimeout(() => {
+          navigate('/dashboard');
+        }, delayInMilliseconds);
+        return () => clearTimeout(timeoutId);
+      }
+      catch (error) {
+      if(error.response.statusText === "Not Found"){
+        toast('Password does not match😔!');
+      }
+      console.error('API error:', error.response);
+    }
+  };
+
+return (
+  <CoverLayout
+    title="Welcome back"
+    description="Enter your email and password to sign in"
+    image={curved9}
+  >
+    <ToastContainer
+      position="bottom-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+    <SoftBox component="form" role="form" onSubmit={handleFormSubmit}>
+      <SoftBox mb={2}>
+        <SoftBox mb={1} ml={0.5}>
+          <SoftTypography component="label" variant="caption" fontWeight="bold">
+            Email
           </SoftTypography>
         </SoftBox>
-        <SoftBox mt={4} mb={1}>
-          <Link to={"/dashboard"}>
-          <SoftButton variant="gradient" color="info" onClick = {()=> localStorage.setItem("user", "user")} fullWidth>
+        <SoftInput type="email" placeholder="Email" name="email" onChange={handleInputChange}/>
+      </SoftBox>
+      <SoftBox mb={2}>
+        <SoftBox mb={1} ml={0.5}>
+          <SoftTypography component="label" variant="caption" fontWeight="bold">
+            Password
+          </SoftTypography>
+        </SoftBox>
+        <SoftInput type="password" placeholder="Password" name="password" onChange={handleInputChange}/>
+      </SoftBox>
+      <SoftBox display="flex" alignItems="center">
+        <Switch checked={rememberMe} onChange={handleSetRememberMe} />
+        <SoftTypography
+          variant="button"
+          fontWeight="regular"
+          onClick={handleSetRememberMe}
+          sx={{ cursor: "pointer", userSelect: "none" }}
+        >
+          &nbsp;&nbsp;Remember me
+        </SoftTypography>
+      </SoftBox>
+      <SoftBox mt={4} mb={1}>
+          <SoftButton variant="gradient" color="info" type="submit" fullWidth>
             sign in
           </SoftButton>
-          </Link>
-        </SoftBox>
-        <SoftBox mt={3} textAlign="center">
-          <SoftTypography variant="button" color="text" fontWeight="regular">
-            Don&apos;t have an account?{" "}
-            <SoftTypography
-              component={Link}
-              to="/authentication/sign-up"
-              variant="button"
-              color="info"
-              fontWeight="medium"
-              textGradient
-            >
-              Sign up
-            </SoftTypography>
-          </SoftTypography>
-        </SoftBox>
       </SoftBox>
-    </CoverLayout>
-  );
+      <SoftBox mt={3} textAlign="center">
+        <SoftTypography variant="button" color="text" fontWeight="regular">
+          Don&apos;t have an account?{" "}
+          <SoftTypography
+            component={Link}
+            to="/authentication/sign-up"
+            variant="button"
+            color="info"
+            fontWeight="medium"
+            textGradient
+          >
+            Sign up
+          </SoftTypography>
+        </SoftTypography>
+      </SoftBox>
+    </SoftBox>
+  </CoverLayout>
+);
 }
 
 export default SignIn;
--- campus_cycle/src/layouts/authentication/sign-up/index.js
+++ campus_cycle/src/layouts/authentication/sign-up/index.js
@@ -1,44 +1,56 @@
 
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import React from "react";
 // react-router-dom components
 import { Link } from "react-router-dom";
 
 // @mui material components
 import Card from "@mui/material/Card";
-import Checkbox from "@mui/material/Checkbox";
 
 // Soft UI Dashboard React components
 import SoftBox from "components/SoftBox";
 import SoftTypography from "components/SoftTypography";
 import SoftInput from "components/SoftInput";
 import SoftButton from "components/SoftButton";
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 
 // Authentication layout components
 import BasicLayout from "layouts/authentication/components/BasicLayout";
 import ImageUpload from "layouts/discussion/components/image_upload";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+
 
 // Images
 import curved6 from "assets/images/curved-images/curved14.jpg";
+import LOCAL_ADDR from "GLOBAL_ADDRESS";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function SignUp() {
-  const [agreement, setAgremment] = useState(true);
-  const [img, setImg] = useState();
-  // console.log(img);
-  // body.name,
-  //     body.email,
-  //     body.dob,
-  //     body.password,
-  //     body.imageUrl,
-  //     body.universityId,
-  //     body.phoneNo,
+  const navigate = useNavigate();
 
-  const handleSetAgremment = () => setAgremment(!agreement);
+  const [agreement, setAgremment] = useState(true);
+  const [uni, setUni] = useState(null);
+  const [pass, setPass] = useState('');
+  const [cpass, setCpass] = useState('');
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${LOCAL_ADDR}universities`);
+        setUni(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  // console.log(uni);
+
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -52,32 +64,46 @@ function SignUp() {
     const { name, value } = e.target;
     // console.log(name);
     setFormData({ ...formData, [name]: value });
-    // console.log(formData);
+    // fetchData();
+    // console.log(pass);
   };
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      // Replace 'YOUR_API_ENDPOINT' with your actual backend API endpoint
-      const response = await axios.post(`signup`, formData);
-      // console.log('API response:', response.data);
+    if (cpass == pass) {
+      formData.password = pass;
+      formData.phoneNo = formData.phoneNo;
 
-      // Clear form after successful submission
-      setFormData({
-        name: '',
-        email: '',
-        password: '',
-        dob: '',
-        imageUrl: '',
-        universityId: null,
-        phoneNo: ''
-      });
+      formData.dob = new Date(formData.dob).toISOString();
 
-      // Handle success (e.g., redirect user, show success message)
-    } catch (error) {
-      console.error('API error:', error);
-      // Handle error (e.g., show error message)
+      formData.universityId = parseInt(formData.universityId);
+      // console.log(formData);
+      try {
+        const response = await axios.post(`${LOCAL_ADDR}users/signup`, formData);
+        setFormData({
+          name: '',
+          email: '',
+          password: '',
+          dob: '',
+          universityId: null,
+          phoneNo: ''
+        });
+        if (response.statusText === "Created") {
+          toast('Successfully signed up😎!');
+            const delayInMilliseconds = 5000;
+        
+            const timeoutId = setTimeout(() => {
+              navigate('/');
+            }, delayInMilliseconds);
+            return () => clearTimeout(timeoutId);
+        }
+      } catch (error) {
+        console.error('API error:', error);
+      }
+    }
+    else {
+      toast('Passwords do not match😔!');
     }
   };
 
@@ -87,6 +113,18 @@ function SignUp() {
       description="Use these awesome forms to login or create new account in your project for free."
       image={curved6}
     >
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        />
       <Card>
         <SoftBox p={3} mb={1} textAlign="center">
           <SoftTypography variant="h5" fontWeight="medium">
@@ -98,84 +136,61 @@ function SignUp() {
         </SoftBox> */}
         {/* <Separator /> */}
 
-        <form onSubmit={handleFormSubmit}>
-          <SoftBox pt={2} pb={3} px={3}>
-            <SoftBox component="form" role="form">
-              <SoftBox mb={2}>
-                <SoftInput placeholder="Name" name="name" value={formData.name} onChange={handleInputChange} />
-              </SoftBox>
-              <SoftBox mb={2}>
-                <SoftInput type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Email" />
-              </SoftBox>
-              <SoftBox mb={2}>
-                <select style={{ width: "100%", height: 40, borderRadius: 10, borderColor: '#bebebe', transitionTimingFunction: 'ease-in-out' }}>
-                  <option value="someOption">Some option</option>
-                  <option value="otherOption">Other option</option>
-                </select>
-              </SoftBox>
-              <SoftBox mb={2}>
-                <SoftInput placeholder="Mobile" type="number" />
-              </SoftBox>
-              <SoftBox mb={2}>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DemoContainer components={['DatePicker']}>
-                    <label style={{ fontSize: 14, alignSelf: "center", color: "lightgray" }}>DOB</label>
-                    <DatePicker sx={{ marginLeft: "auto" }} />
-                  </DemoContainer>
-                </LocalizationProvider>
-              </SoftBox>
-              <SoftBox mb={2}>
-                <SoftInput type="password" placeholder="Password" />
-              </SoftBox>
-              <SoftBox mb={2}>
-                <SoftInput type="password" placeholder="Confirm Password" />
-              </SoftBox>
-              <SoftBox mb={2}>
-                <ImageUpload setImg={setImg} />
-              </SoftBox>
-              {/* <SoftBox display="flex" alignItems="center">
-              <Checkbox checked={agreement} onChange={handleSetAgremment} />
-              <SoftTypography
-                variant="button"
-                fontWeight="regular"
-                onClick={handleSetAgremment}
-                sx={{ cursor: "poiner", userSelect: "none" }}
+        <SoftBox pt={2} pb={3} px={3}>
+          <SoftBox component="form" role="form" onSubmit={handleFormSubmit}>
+            <SoftBox mb={2}>
+              <SoftInput placeholder="Name" name="name" value={formData.name} onChange={handleInputChange} />
+            </SoftBox>
+            <SoftBox mb={2}>
+              <SoftInput type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Email" />
+            </SoftBox>
+            <SoftBox mb={2}>
+              <select
+                style={{ width: "100%", height: 40, borderRadius: 10, borderColor: '#bebebe', transitionTimingFunction: 'ease-in-out', padding: 10 }}
+                name="universityId"
+                onClick={handleInputChange}
               >
-                &nbsp;&nbsp;I agree the&nbsp;
-              </SoftTypography>
-              <SoftTypography
-                component="a"
-                href="#"
-                variant="button"
-                fontWeight="bold"
-                textGradient
-              >
-                Terms and Conditions
-              </SoftTypography>
-            </SoftBox> */}
-              <SoftBox mt={4} mb={1}>
-                <SoftButton variant="gradient" color="dark" fullWidth>
-                  sign up
-                </SoftButton>
-              </SoftBox>
-              <SoftBox mt={3} textAlign="center">
-                <SoftTypography variant="button" color="text" fontWeight="regular">
-                  Already have an account?&nbsp;
-                  <SoftTypography
-                    component={Link}
-                    to="/authentication/sign-in"
-                    variant="button"
-                    color="dark"
-                    fontWeight="bold"
-                    textGradient
-                  >
-                    Sign in
-                  </SoftTypography>
+                {
+                  uni?.map((item) => {
+                    return <option key={item?.id} value={item?.id} style={{ padding: 20 }}>{item?.fullName}</option>
+                  })
+                }
+              </select>
+            </SoftBox>
+            <SoftBox mb={2}>
+              <SoftInput placeholder="Mobile" type="text" name="phoneNo" onChange={handleInputChange} value={formData.phoneNo} />
+            </SoftBox>
+            <SoftBox mb={2}>
+              <SoftInput type="date" name="dob" value={formData.dob} onChange={handleInputChange} />
+            </SoftBox>
+            <SoftBox mb={2}>
+              <SoftInput type="password" placeholder="Password" onChange={(e) => setPass(e.target.value)} />
+            </SoftBox>
+            <SoftBox mb={2}>
+              <SoftInput type="password" placeholder="Confirm Password" onChange={e => setCpass(e.target.value)} />
+            </SoftBox>
+            <SoftBox mt={4} mb={1}>
+              <SoftButton variant="gradient" color="dark" fullWidth type="submit">
+                sign up
+              </SoftButton>
+            </SoftBox>
+            <SoftBox mt={3} textAlign="center">
+              <SoftTypography variant="button" color="text" fontWeight="regular">
+                Already have an account?&nbsp;
+                <SoftTypography
+                  component={Link}
+                  to="/authentication/sign-in"
+                  variant="button"
+                  color="dark"
+                  fontWeight="bold"
+                  textGradient
+                >
+                  Sign in
                 </SoftTypography>
-              </SoftBox>
+              </SoftTypography>
             </SoftBox>
           </SoftBox>
-        </form>
+        </SoftBox>
       </Card>
     </BasicLayout>
   );
